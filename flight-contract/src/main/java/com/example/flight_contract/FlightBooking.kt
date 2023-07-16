b41fa19ffb112c6f20ee693d525752b722a757f6
rindang <rindang2t7@gmail.com>
2023-07-16 17:51:42 +0700

    ++ adding contract on each dfm ++ adding context to generate view in the contract

--- flight-contract/src/main/java/com/example/flight_contract/FlightBooking.kt
+++ flight-contract/src/main/java/com/example/flight_contract/FlightBooking.kt
@@ -1,9 +1,10 @@
 package com.example.flight_contract
 
+import android.content.Context
 import android.view.View
 
 interface FlightBooking {
-    fun getForm(user: FlightUser) : View
+    fun getForm(user: FlightUser, context: Context) : View
 }
 
 data class FlightUser(val name :String)
--- hotel-contract/src/main/java/com/example/hotel_contract/HotelBooking.kt
+++ hotel-contract/src/main/java/com/example/hotel_contract/HotelBooking.kt
@@ -1,9 +1,10 @@
 package com.example.hotel_contract
 
+import android.content.Context
 import android.view.View
 
 interface HotelBooking {
-    fun getForm(user: HotelUser) : View
+    fun getForm(user: HotelUser, context: Context) : View
 }
 
 data class HotelUser(val name: String)
